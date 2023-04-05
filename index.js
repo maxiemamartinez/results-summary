b40fa2d56b2c9e921329760190979037347a489b
martinezmaxiema@gmail.com <maximiliano@Maximilianos-MacBook-Pro.local>
2023-04-05 13:18:47 -0300

    test

---
+++ index.js
@@ -0,0 +1,87 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Website</title>
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      relo="noopener"
+      target="_blank"
+      href="./styles.css"
+    />
+  </head>
+
+  <body>
+    <main>
+      <div class="container">
+        <div class="box result">
+          <h1 class="title"> Your Result</h1>
+          <div class="circle">
+            <h2 class="mid-text">76</h2>
+            <p class="mid-p">of 100</p>
+          </div>
+          <div class="explain">
+            <h2 class="end-text"> Great </h2>
+            <p class="end-p">
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
+          </div>
+        </div>
+        <div class="box summary">
+          <h1 class="title-summary">Summary</h1>
+          <div class="padre">
+            <div class="react">
+              <div class="reaction">
+                <img class="icon" src="./assets/icons/icon-reaction.svg" />
+                <span>Reaction</span>
+              </div>
+              <div class="reac-result">
+                <span class="bolder">80</span>
+                <span>/</span>
+                <span>100</span>
+              </div>
+            </div>
+            <div class="memory">
+              <div class="mem">
+                <img class="icon" src="./assets/icons/icon-memory.svg" />
+                <span>Memory</span>
+              </div>
+              <div class="mem-result">
+                <span class="bolder">92</span>
+                <span>/</span>
+                <span>100</span>
+              </div>
+            </div>
+            <div class="verbal">
+              <div class="verb">
+                <img class="icon" src="./assets/icons/icon-verbal.svg" />
+                <span>Verbal</span>
+              </div>
+              <div class="verb-result">
+                <span class="bolder">61</span>
+                <span>/</span>
+                <span>100</span>
+              </div>
+            </div>
+            <div class="visual">
+              <div class="vis">
+                <img class="icon" src="./assets/icons/icon-visual.svg" />
+                <span>Visual</span>
+              </div>
+              <div class="vis-result">
+                <span class="bolder">72</span> <span>/</span>
+                <span>100</span>
+              </div>
+            </div>
+          </div>
+          <button class="btn">Continue</button>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>;
